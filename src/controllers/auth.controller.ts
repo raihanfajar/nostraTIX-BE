afc68566cde3ce75a8af277ed6ecb6af2360ac35
ghazianshari <ghazi.anshari@gmail.com>
2025-07-29 13:23:54 +0700

    feat(auth): initiate auth route, controller, and service for user and organizer

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from "express";
+import {
+    loginOrganizerService,
+    loginUserService,
+    registerOrganizerService,
+    registerUserService
+} from "../services/auth.service";
+
+export const registerUserController = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const result = await registerUserService(req.body);
+        res.status(201).send({ result });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const loginUserController = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const result = await loginUserService(req.body);
+        res.status(200).send({ result });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const registerOrganizerController = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const result = await registerOrganizerService(req.body);
+        res.status(201).send({ result });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const loginOrganizerController = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const result = await loginOrganizerService(req.body);
+        res.status(200).send({ result });
+    } catch (error) {
+        next(error);
+    }
+};
