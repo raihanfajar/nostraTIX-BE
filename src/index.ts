2d4794d40053994694e41ea70a19c71ba52fbca1
raihanfajar <rhnfjr@gmail.com>
2025-08-15 16:08:18 +0700

    fix: fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-import 'dotenv/config';
+// import 'dotenv/config';
+
+// LANGKAH 1: Lakukan tes diagnostik di baris paling pertama
+console.log("DIAGNOSTIC LOG AT TOP:", process.env.DATABASE_URL);
+
+// LANGKAH 2: Hanya jalankan dotenv di lingkungan development
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+
 import express from "express";
 import cors from "cors";
 import mainRouter from "./routers/index.route";
